2fcdccb3ae2432739e9de29100828446e8e102a4
Elizabeth <mwendemuthusi6@gmail.com>
2022-12-10 22:09:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-function isPalindrome(word) {
-  // Write your algorithm here
-}
 
-/* 
-  Add your pseudocode here
+function isPalindrome(word) {
+  let new1 =[];
+  let new2 =[];
+  let z;
+  let y;
+  let midNumberIndex = Math.floor(word.length/2)
+  let x = word.split('')
+
+  if(word.length % 2 == 0){
+    for(char in x){
+    if(char<midNumberIndex){
+      new1.push(x[char])
+      y = new1.join('');
+    }else{
+      new2.unshift(x[char])
+      z = new2.join('');
+    }
+  }
+  }else{
+    for(char in x){
+    if(char<midNumberIndex){
+      new1.push(x[char])
+      y = new1.join('');
+    }else if(char>midNumberIndex){
+      new2.unshift(x[char])
+      z = new2.join('');
+    }
+  }
+  }
+  return y===z;
+}
+isPalindrome("abba")
+
+/*
+initialize two empty arrays and two variables;
+
+find mid number index and assign midNumberIndex
+split the word into an array and assign x;
+
+depending on if word is even or odd, loop over the split word(x);
+if element is less than midNumberIndex, push into one empty array and join while assigning a variable(y);
+if element is equal or more than midNumberIndex(depending), unshift into the other empty array and join while assigning a variable(z);
+
+return y === z;
+
 */
 
 /*
-  Add written explanation of your solution here
+A palindrome can have odd or even numbers in length and repeat each other forward and backward from the mid.
+My solution aims to split the word into two and turn one side of the split to reverse and compare the two split ends using strict equality operator
 */
 
 // You can run `node index.js` to view these console logs
